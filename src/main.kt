22228577de70da6060440a4987130fcc1d20bdb4
Raphael.AmorimAlex <Raphael.amorimalex@gmail.com>
2021-02-22 19:53:50 -0300

    Aula 7 operaçoes com Strings

---
+++ src/main.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val nome = "Raphael"
+    val sobrenome = "Alexander Moura Amorim"
+    println(nome + sobrenome)
+    println("${sobrenome} , ${nome}")
+    println("Bem vindo ${nome}")
+}
